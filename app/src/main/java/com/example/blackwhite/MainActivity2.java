f696c5dfaa628054c6ea3289b68d412dce88bf44
MarinaManushina <122769289+MarinaManushina@users.noreply.github.com>
2023-04-22 08:40:07 +0300

    add

--- app/src/main/java/com/example/blackwhite/MainActivity2.java
+++ app/src/main/java/com/example/blackwhite/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.blackwhite;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
